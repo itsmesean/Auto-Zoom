bd71c3e2d3a2636013d3e59e5c4a9eaedb5e51f7
Sean arseneault <itsmesean@pm.me>
2020-10-09 23:17:16 -0700

    Meetings list

--- background.js
+++ background.js
@@ -1,27 +1,27 @@
-let timerID;
-let meetTime;
-let url;
+let meetings = {};
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.cmd === 'START_TIMER') {
-    meetTime = request.when;
-    url = request.link;
-    
-     setTimeout(() => {
-      chrome.tabs.create({ url: url })
+  if (request.cmd === "START_TIMER") {
+    let meetTime = Date.parse(request.when);
+    let url = request.link;
+    meetings[request.when] = url;
+
+    setTimeout(() => {
+      delete meetings[request.when];
+      chrome.tabs.create({ url: url, active: true }, (tab) => {
+        setTimeout(function(){
+          chrome.tabs.remove(tab.id);
+      },500);
+      });
     }, meetTime - Date.now());
-  } 
+  }
+
+  if (request.cmd === "GET_MEETINGS") {
+    sendResponse(meetings);
+  }
+  
+  if (request.cmd === "REMOVE_ITEM") {
+    delete meetings[request.id];
+    sendResponse(meetings)
+  }
 });
-
-// Make popup clickable on any url -- must be a better way to do this
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
-
--- popup.js
+++ popup.js
@@ -1,21 +1,86 @@
+const submitMeeting = function () {
+  let inputTime = document.querySelector('input[type="datetime-local"]');
+  let url = document.getElementById("url-input");
+  let alert = document.getElementById("url-alert");
+  alert ? alert.remove() : null;
 
-  const timeChecker = function() {
-    let inputTime = document.querySelector('input[type="datetime-local"]');
-    let url = document.getElementById('url-input').value;
-    console.log(url)
-    let meetTime = Date.parse(inputTime.value)
-    startTime(meetTime, url)
+  if (urlValidator(url.value)) {
+    chrome.runtime.sendMessage({
+      cmd: "START_TIMER",
+      when: inputTime.value,
+      link: url.value,
+    });
+    getMeetings();
+  } else {
+    document
+      .getElementById("url-alert-div")
+      .insertAdjacentHTML(
+        "afterbegin",
+        `<p id="url-alert">Please enter a valid Zoom link</p>`
+      );
   }
-  
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.cmd === 'DONE') {
-      window.location = request.link
-    } 
+};
+
+const getMeetings = function () {
+  chrome.runtime.sendMessage({ cmd: "GET_MEETINGS" }, function (response) {
+    populate(response);
   });
+};
 
-document.getElementById("submit-button").onclick = function() {timeChecker()};
+const populate = function (items) {
+  document.getElementById("meetings-list").innerHTML = "";
+  for (meeting in items) {
+    let date = new Date(meeting);
+    let element = document.getElementById("meetings-list");
+    element.insertAdjacentHTML(
+      "afterbegin",
+      `<li 
+        class="meeting-li" 
+        id=${meeting}>
+        ${formatDate(date)}  
+        ${items[meeting]}
+        <button id='remove-li'>X</button>
+      </li>`
+    );
+  }
+};
 
-function startTime(time, url) {
-  chrome.runtime.sendMessage({ cmd: 'START_TIMER', when: time, link: url });
-}
+const formatDate = function (date) {
+  const timeOptions = {
+    hour12: true,
+    hour: "numeric",
+    minute: "2-digit",
+  };
 
+  const dateOptions = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  };
+
+  return date.toLocaleString("en-US", { ...timeOptions, ...dateOptions });
+};
+
+const urlValidator = function (url) {
+  let regex = /^https:\/\/zoom\.us\/j\/\d{5,}/gm;
+  return regex.test(url);
+};
+
+document.getElementById("submit-button").onclick = function (e) {
+  e.preventDefault();
+  submitMeeting();
+};
+
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "remove-li") {
+    e.preventDefault();
+    chrome.runtime.sendMessage(
+      { cmd: "REMOVE_ITEM", id: e.target.parentElement.id },
+      function (response) {
+        populate(response);
+      }
+    );
+  }
+});
+
+chrome.runtime.onStartup.addListener(getMeetings());
